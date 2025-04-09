baa888038ae3d3c9cae262f7e733499acad222dc
BrianNixDesktop <briannormant@gmail.com>
2025-04-09 02:00:55 -0400

    Nix: update flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,66 @@
 {
-	description = "TicTacToe";
+	description = "A giant tic tac toe solving program";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		flake-parts.url = "github:hercules-ci/flake-parts";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs, ... }: let
-		system = "x86_64-linux";
-		pkgs = import nixpkgs { inherit system; };
-	in {
-		devShells."${system}".default = pkgs.mkShell {
-			packages = with pkgs; [
-				gradle
-				jdk21
-				zsh
+	outputs = inputs@{flake-parts, ... }: let
+			project_name = "TicTacToe";
+		in flake-parts.lib.mkFlake {inherit inputs;} {
+			imports = [
+				# flake module to import
 			];
-			shellHook = ''
-                export SHELL=zsh
-				if [[ $TMUX ]]; then
-					tmux rename-window "nvim"
-					tmux rename-session "TicTacToe"
-				fi
-				exec zsh
-			'';
+			systems = ["x86_64-linux"];
+			perSystem = {config, pkgs, system, ... }: {
+				devShells.default = pkgs.mkShell {
+					packages = with pkgs; [
+						zsh
+						tmux
+						gradle
+					];
+					shellHook = ''
+						export SHELL=zsh
+						SESSION_NAME="${project_name}"
+						DEV_DIR=$PWD
+
+						# Check if session exists
+						if tmux has-session -t "$SESSION_NAME" > /dev/null 2>&1; then
+							echo "Tmux $SESSION_NAME session already running."
+							tmux switch-client -t "$SESSION_NAME":1
+							exit 0
+						fi
+
+						# Check if session exists but is detached
+						if tmux list-sessions | grep -q "$SESSION_NAME"; then
+							echo "Tmux $SESSION_NAME session exists, attaching..."
+							tmux attach-session -t "$SESSION_NAME":1
+							exit 0
+						fi
+
+						tmux new-session -d -c "~" -s $SESSION_NAME
+
+						tmux respawn-window -k -t $SESSION_NAME:1 -c $DEV_DIR "nix develop .#editor"
+						tmux rename-window -t $SESSION_NAME:1 "editor"
+
+						tmux new-window -t $SESSION_NAME:2 -n "git" -c $DEV_DIR "lazygit"
+						tmux split-window -t $SESSION_NAME:2 -c $DEV_DIR
+
+						tmux switch -t $SESSION_NAME:1
+
+						exit 0;
+					'';
+				};
+				devShells.editor = pkgs.mkShell {
+					packages = with pkgs; [ gradle zsh ];
+					shellHook = ''
+						export PROJECT=TicTacToe
+						export SHELL=zsh
+						exec neovide
+					'';
+				};
+			};
+			flake = {};
 		};
-	};
 }
